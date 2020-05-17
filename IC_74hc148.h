8d9839f795ffb3d2b40edefee4ee390b1b17ea3f
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-05-17 17:26:20 +0530

    changed __uint8_t to uint8_t

--- IC_74hc148.h
+++ IC_74hc148.h
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include<stdint.h>
+#include<stdbool.h> 
 
 class IC_74hc148{
     public:
-        void Init(volatile __uint8_t *pport_c,__uint8_t data_c,
-                    volatile __uint8_t *pport_b,__uint8_t data_b,
-                    volatile __uint8_t *pport_a,__uint8_t data_a);
+        void Init(volatile uint8_t *pport_c,uint8_t data_c,
+                    volatile uint8_t *pport_b,uint8_t data_b,
+                    volatile uint8_t *pport_a,uint8_t data_a);
         int read();
     private:
-        volatile __uint8_t *port_c, c, *port_b, b, *port_a, a;
-        volatile __uint8_t *port2DDR(volatile __uint8_t *port){return port-1;}
+        volatile uint8_t *port_c, c, *port_b, b, *port_a, a;
+        volatile uint8_t *port2DDR(volatile uint8_t *port){return port-1;}
 };
